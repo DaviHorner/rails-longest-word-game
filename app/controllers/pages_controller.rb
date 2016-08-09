5c1834745c78b2b24f1fd8b6ba9ffe5ce021f87a
Davi Horner <davihhcastro@gmail.com>
2016-08-09 09:01:23 -0300

    Add rails

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,85 @@
+require 'open-uri'
+require 'json'
+
+class PagesController < ApplicationController
+
+  before_action :setup_session
+
+  def game
+    @gen_grid = generate_grid(9)
+    setup_session
+    session[:grid] = @gen_grid.join
+    session[:start_time] = Time.now.to_i
+  end
+
+  def score
+    # 1. Get the users guess
+    @guess = params[:guess]
+    # x. Get the grid
+    grid = session[:grid].split("")
+    # x. Get start time
+    @start = session[:start_time]
+    # x. Get end time
+    end_time = Time.now
+    # 2. Compute user score
+    @score = run_game(@guess, grid, @start, end_time)
+    session[:score] += @score[:score]
+  end
+
+
+  private
+
+  def setup_session
+    session[:score] ||= 0
+  end
+
+
+  def generate_grid(grid_size)
+    Array.new(grid_size) { ('A'..'Z').to_a[rand(26)] }
+  end
+
+
+  def included?(guess, grid)
+    the_grid = grid.clone
+    guess.chars.each do |letter|
+      the_grid.delete_at(the_grid.index(letter)) if the_grid.include?(letter)
+    end
+    grid.size == guess.size + the_grid.size
+  end
+
+  def compute_score(attempt, time_taken)
+    (time_taken > 60.0) ? 0 : attempt.size * (1.0 - time_taken / 60.0)
+  end
+
+  def run_game(attempt, grid, start_time, end_time)
+    result = { time: end_time - start_time }
+
+    result[:translation] = get_translation(attempt)
+    result[:score], result[:message] = score_and_message(
+      attempt, result[:translation], grid, result[:time])
+    result
+  end
+
+  def score_and_message(attempt, translation, grid, time)
+    if translation
+      if included?(attempt.upcase, grid)
+        score = compute_score(attempt, time)
+        [score, "well done"]
+      else
+        [0, "not in the grid"]
+      end
+    else
+      [0, "not an english word"]
+    end
+  end
+
+
+  def get_translation(word)
+    response = open("http://api.wordreference.com/0.8/80143/json/enfr/#{word.downcase}")
+    json = JSON.parse(response.read.to_s)
+    json['term0']['PrincipalTranslations']['0']['FirstTranslation']['term'] unless json["Error"]
+  end
+
+end
+
+
